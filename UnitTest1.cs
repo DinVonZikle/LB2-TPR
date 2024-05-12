3e9dab83e0a9c7c7c1edcebc954591f3e1e7f0fb
DinVonZikle <119157761+DinVonZikle@users.noreply.github.com>
2024-05-12 17:59:13 +0300

    Add files via upload

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -8,13 +8,21 @@ namespace TestPayrollCalculator
         [TestMethod]
         public void TestThreeEmployeesSum()
         {
-            var payrollCalculator = new PayrollCalculator();
-
             var employees = new List<Employee> {
                 new Employee { Name = "John", Position = "manager", Salary = 5000 },
                 new Employee { Name = "Doe", Position = "developer", Salary = 4000 },
                 new Employee { Name = "Jane", Position = "intern", Salary = 2000 }
             };
+
+            var positions = new List<Position>()
+            {
+                new Position { PositionName = "manager", SalaryCoefficient = 1.5m },
+                new Position { PositionName = "developer", SalaryCoefficient = 1.2m },
+                new Position { PositionName = "intern", SalaryCoefficient = 1.0m }
+            };
+
+            var payrollCalculator = new PayrollCalculator(positions);
+
 
             var totalPayroll = payrollCalculator.CalculateTotalPayroll(employees);
 
@@ -22,17 +30,47 @@ namespace TestPayrollCalculator
         }
 
         [TestMethod]
+        [ExpectedException(typeof(InvalidUserPositionException))]
         public void TestZeroEmployeesSum()
         {
-            var payrollCalculator = new PayrollCalculator();
-
-            var employees = new List<Employee>
-            {
-
+            var employees = new List<Employee> {
+                new Employee { Name = "John", Position = "manager", Salary = 5000 },
+                new Employee { Name = "Doe", Position = "developer", Salary = 4000 },
+                new Employee { Name = "Jane", Position = "someone", Salary = 2000 }
             };
-            var totalPayroll = payrollCalculator.CalculateTotalPayroll(employees);
 
-            Assert.AreEqual(0, totalPayroll);
+            var positions = new List<Position>()
+            {
+                new Position { PositionName = "manager", SalaryCoefficient = 1.5m },
+                new Position { PositionName = "developer", SalaryCoefficient = 1.2m },
+                new Position { PositionName = "intern", SalaryCoefficient = 1.0m }
+            };
+
+            var payrollCalculator = new PayrollCalculator(positions);
+
+            var totalPayroll = payrollCalculator.CalculateTotalPayroll(employees);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidSalaryException))]
+        public void TestInvalidSalaryInput()
+        {
+            var employees = new List<Employee> {
+                new Employee { Name = "John", Position = "manager", Salary = 5000 },
+                new Employee { Name = "Doe", Position = "developer", Salary = 4000 },
+                new Employee { Name = "Jane", Position = "intern", Salary = -1242 }
+            };
+
+            var positions = new List<Position>()
+            {
+                new Position { PositionName = "manager", SalaryCoefficient = 1.5m },
+                new Position { PositionName = "developer", SalaryCoefficient = 1.2m },
+                new Position { PositionName = "intern", SalaryCoefficient = 1.0m }
+            };
+
+            var payrollCalculator = new PayrollCalculator(positions);
+
+            var totalPayroll = payrollCalculator.CalculateTotalPayroll(employees);
         }
     }
 }
